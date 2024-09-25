4b68f067e9ec29bfa786b5ffa6e04bda5d30e4a0
Benson-Zheng-ICD2O1-02 <benson.zheng3@student.tdsb.on.ca>
2024-09-25 19:18:35 +0000

    888

--- PP1_7.py
+++ PP1_7.py
@@ -2,24 +2,42 @@
 
 def q1():
   #Write code here
+   bool1 = "true"
+   print(bool1)
 
 def q2():
   #Write code here
+  num1 = input("Input an integer: ")
+  num1 = int(num1)
+  bool1 = num1 > 5
+  print(bool1)
+
 
 def q3():
   #Write code here
+  let1 = input("Input the letter a: ")
+  let1 = str(let1)
+  bool1 = let1 == 'a'
+  print(bool1)
 
 def q4():
   #Write code here
-
+  word1 = input("Input a word earlier in the dictionary than google: ")
+  bool1 = word1 < "google"
+  print(bool1)
 def q5():
   #Write code here
-
+  num2 = input("Input an integer: ")
+  num3 = input("Input another integer ")
+  num2 = int(num2)
+  num3 = int(num3)
+  bool1 = (num3*num2) > 40
+  print (f"Your numbers multiplied together are greater than 40: ", bool1)
 #Do edit the code below
 #Comment the lines below when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
